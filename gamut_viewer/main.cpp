6ba4f105542cb3a2f1b0e51b84db9ed2f8279b81
the-last-willy <willy.jacquet@outlook.com>
2023-03-06 14:44:48 +0100

    Struggle with shader compilation logs.

--- gamut_viewer/main.cpp
+++ gamut_viewer/main.cpp
@@ -6,29 +6,104 @@
 #include <imgui.h>
 
 #include <iostream>
+#include <fstream>
+#include <optional>
+#include <string>
+#include <vector>
 
-void
-onOpenGLDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity,
-					 GLsizei length, const GLchar *message,
-					 const void *userParam) {
+std::string fileContent(std::string path) {
+	std::ifstream ifs(path);
+	if(!ifs.is_open()) {
+		throw std::runtime_error("Failed to open file.");
+	} else {
+		return {
+			std::istreambuf_iterator<char>(ifs),
+			std::istreambuf_iterator<char>()
+		};
+	}
+}
 
-	(void) source, type, id, severity, length, message, userParam;
-	std::cerr << "OpenGL: " << message << std::endl;
+auto optCompilationError(GLuint shader) -> std::optional<std::string> {
+	GLint status = 0;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if(status == GL_TRUE)
+		return std::nullopt;
+	else
+	{
+		GLint log_length = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+		std::string log(log_length, '\0');
+		glGetShaderInfoLog(shader, log_length, nullptr, data(log));
+		return log;
+	}
+}
+
+auto optLinkageError(GLuint program) -> std::optional<std::string> {
+	GLint status = 0;
+	glGetProgramiv(program, GL_LINK_STATUS, &status);
+	if(status == GL_TRUE)
+		return std::nullopt;
+	else
+	{
+		GLint log_length = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length);
+		std::string log(log_length, '\0');
+		glGetProgramInfoLog(program, log_length, nullptr, data(log));
+		return log;
+	}
 }
 
 int main() {
 	auto window = Window();
 	window.resize(1280, 720);
 
-	glEnable(GL_DEBUG_OUTPUT);
-	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-	glDebugMessageCallback(&onOpenGLDebugMessage, nullptr);
+//	GLuint vbo = 0;
+//	glGenBuffers(1, &vbo);
+//	auto vertices = std::vector<float>{
+//		0.f, 0.f, 0.f,
+//		1.f, 0.f, 0.f,
+//		0.f, 1.f, 0.f,
+//	};
+//	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+//	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * size(vertices),
+//				 data(vertices), GL_STATIC_DRAW);
+//
+//	GLuint vao = 0;
+//	// glGenVertexArrays(1, &vao);
+//	glBindVertexArray(vao);
+//	glVertexAttribFormat(0, 3, GL_FLOAT, GL_FALSE, 0);
+//	glBindVertexBuffer(0, vbo, 0, 3 * sizeof(float));
+//	glVertexAttribBinding(0, 0);
+//	glEnableVertexAttribArray(0);
 
-	GLuint vao = 0;
-	glGenVertexArrays(1, &vao);
-
-	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
+	auto vert_shader = glCreateShader(GL_VERTEX_SHADER);
+//	{
+//		auto vert_source = fileContent(
+//			"D:\\dev\\project\\gamut_viewer\\gamut_viewer\\forward.vert");
+//		auto str = data(vert_source);
+//		auto size = static_cast<GLint>(std::size(vert_source));
+//		glShaderSource(vert_shader, 1, &str, &size);
+//		glCompileShader(vert_shader);
+//	}
+//
+//	auto frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+//	{
+//		auto source = fileContent(
+//			"D:\\dev\\project\\gamut_viewer\\gamut_viewer\\forward.frag");
+//		auto str = data(source);
+//		auto size = static_cast<GLint>(std::size(source));
+//		glShaderSource(frag_shader, 1, &str, &size);
+//		glCompileShader(frag_shader);
+//		GLint status;
+//		glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &status);
+//	}
+//
+//	auto shader_prog = glCreateProgram();
+//	glAttachShader(shader_prog, vert_shader);
+//	glAttachShader(shader_prog, frag_shader);
+//	glLinkProgram(shader_prog);
+//	GLint lstatus;
+//	glGetProgramiv(shader_prog, GL_LINK_STATUS, &lstatus);
 
 	while(window.isOpen()) {
 		window.startRendering();
@@ -36,6 +111,9 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui::ShowDemoWindow();
 
+//		glUseProgram(shader_prog);
+//		glBindVertexArray(vao);
+//		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		window.stopRendering();
 	}
--- gamut_viewer/window.cpp
+++ gamut_viewer/window.cpp
@@ -2,9 +2,19 @@
 
 #include <stdexcept>
 
-#include <glad/glad.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+
+#include <iostream>
+
+void
+onOpenGLDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity,
+					 GLsizei length, const GLchar *message,
+					 const void *userParam) {
+
+	(void) source, type, id, severity, length, message, userParam;
+	std::cerr << "OpenGL: " << message << std::endl;
+}
 
 void Window::init() {
 	/* Initialize the library */
@@ -12,9 +22,10 @@ void Window::init() {
 		throw std::runtime_error("Failed to init GLFW.");
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 
 	/* Create a windowed mode window and its OpenGL context */
 	handle_ = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
@@ -26,19 +37,25 @@ void Window::init() {
 	/* Make the window's context current */
 	glfwMakeContextCurrent(handle_);
 
+	int version = gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+	if(version == 0) {
+		printf("Failed to initialize OpenGL context\n");
+		throw std::runtime_error("Failed to init OpenGL.");
+	}
+
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageCallback(&onOpenGLDebugMessage, nullptr);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL,
+						  GL_TRUE);
+
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO &io = ImGui::GetIO();
 	(void) io;
 
 	ImGui_ImplGlfw_InitForOpenGL(handle_, true);
-	ImGui_ImplOpenGL3_Init("#version 300 es");
-
-	int version = gladLoadGLES2Loader((GLADloadproc) glfwGetProcAddress);
-	if(version == 0) {
-		printf("Failed to initialize OpenGL context\n");
-		throw std::runtime_error("Failed to init OpenGL.");
-	}
+	ImGui_ImplOpenGL3_Init("#version 400");
 }
 
 void Window::startRendering() {
